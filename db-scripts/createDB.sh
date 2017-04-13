5e57481b8436ff33889634b24faf2999759d5d88
jbcodeforce <jerome.boyer@gmail.com>
2017-04-12 21:01:24 -0700

    finalize script

--- db-scripts/createDB.sh
+++ db-scripts/createDB.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
-invdb=$(db2 list db directory | grep Indirect -B 5 |grep "Database alias" |awk {'print $4'} |sort -u | uniq)
+
+# Get the list of databases 
+invdb=$(db2 list db directory | grep Indirect -B 5 |grep "Database alias" |awk {'print $4'} |grep "INVDB")
 echo $invdb
 if [ -z "$invdb" ] ; then 
    echo "DB: INVDB not found so let create it"
@@ -7,8 +9,10 @@ if [ -z "$invdb" ] ; then
    db2 -tvf ../db-sql/create-items.db2 -z mydb.log
 fi
 db2 "connect to INVDB"
-rows=$(db2 -x "select id,name from items" |wc -l)
+db2 -x "select id,name from items" |wc -l >> xx
+read rows < xx 
 echo $rows
+rm xx
 if [[ $rows -eq 0 ]]; then
  echo "There is no data in inventory DB, let add some..."
  db2 -tvf ../db-sql/loaddata.db2 -z mydb.log
@@ -16,5 +20,7 @@ if [[ $rows -eq 0 ]]; then
 fi
 if [[ $rows -lt 12 ]];then
   echo "You do not have all the records"
+else 
+  echo "Good your db is up and populated"
 fi
-echo " Looks good now"
+
