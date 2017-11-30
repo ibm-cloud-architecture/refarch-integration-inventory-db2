685d4918c605146a6f470975b2d2bbd1dd9cb738
Jerome Boyer <boyerje@us.ibm.com>
2017-11-29 21:57:58 -0800

    Example of update table script

--- db-sql/updateTable.sql
+++ db-sql/updateTable.sql
@@ -1,1 +1,15 @@
-CREATE TABLE supplier_delivers_item (SUPPLIERS_ID INTEGER, ITEMS_ID INTEGER) ;
+create table items
+(
+ id INTEGER NOT NULL,
+ name VARCHAR(100) NOT NULL,
+ description VARCHAR(5000),
+ price DECIMAL(8,2),
+ img_alt VARCHAR(200),
+ img VARCHAR(200),
+ model VARCHAR(50),
+ serialNumber VARCHAR(50),
+ producttype VARCHAR(50),
+ CREATIONDATE TIMESTAMP,
+ UPDATEDATE TIMESTAMP,
+ PRIMARY KEY (id)
+);
